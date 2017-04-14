268c8642a5551b663c5314bbadc983a68b806b77
Marcus Tanner <kritiosboy@outlook.com>
2017-04-14 09:26:42 -0600

    Added Triangle with necessary methods, integrated with console output

--- TriangleCalc/Program.cs
+++ TriangleCalc/Program.cs
@@ -10,6 +10,27 @@ namespace TriangleCalc
     {
         static void Main(string[] args)
         {
+            // Get side lengths
+            Console.Write("Length 1: ");
+            int Side1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Length 2: ");
+            int Side2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Length 3: ");
+            int Side3 = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+                Triangle triangle = new Triangle(Side1, Side2, Side3);
+                Console.WriteLine("Perimeter: {0}", triangle.Perimeter());
+                Console.WriteLine("Area: {0}", triangle.Area());
+                Console.WriteLine("Angles: {0}", String.Join(", ", triangle.Angles().Select(x => Math.Truncate(x)) ) );
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.Read();
         }
     }
 }
---
+++ TriangleCalc/Triangle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleCalc
+{
+    class Triangle
+    {
+        private List<int> sides;
+
+        public Triangle(int s1, int s2, int s3)
+        {
+            // Check for length possibilites
+            sides = new List<int>() { s1, s2, s3 };
+            int maxSide = sides.Max();
+            List<int> remainingSides = sides.ToList();
+            remainingSides.Remove(maxSide);
+            if (maxSide >= remainingSides.Sum())
+            {
+                throw new Exception("Cannot create Triangle from given sides.");
+            };
+        }
+
+        public double Area()
+        {
+            double p = Perimeter() / 2.0;
+            double area = Math.Sqrt(p * (p - sides[0]) * (p - sides[1]) * (p - sides[2]));
+            return area;
+        }
+
+        public int Perimeter()
+        {
+            return sides.Sum();
+        }
+
+        public IEnumerable<double> Angles()
+        {
+            List<double> angles = new List<double>();
+
+            // get base and height
+            int baseSide = sides.Max();
+            List<int> remainingSides = sides.ToList();
+            remainingSides.Remove(baseSide);
+            double heightSide = Area() * 2.0 / baseSide;
+            
+            // retrieve angles based on height and side(hypotenuse)
+            double angle1 = Math.Asin(heightSide / remainingSides[0]) * 180 / Math.PI;
+            double angle2 = Math.Asin(heightSide / remainingSides[1]) * 180 / Math.PI;
+            double angle3 = 180 - angle1 - angle2;  
+
+            angles.Add(angle1);
+            angles.Add(angle2);
+            angles.Add(angle3);
+
+            return angles;
+        }
+    }
+}
